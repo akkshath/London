799a2483665c40cc42b04d16a83d7b10cee5d8cc
unknown <sunju1119@gmail.com>
2021-04-22 12:59:23 +0100

    Commit for class test 2

--- london_project.R
+++ london_project.R
@@ -1,3 +1,4 @@
+#question-1
 #loading the data set into the dataframe
 london_crime <- read.csv("london-crime-data.csv",na="")
 str(london_crime)
@@ -11,7 +12,7 @@ converted_date <- paste("01",london_crime$month,london_crime$year, sep = "/")
 converted_date
 
 ###################################
-
+#question-2
 names(london_crime)
 names(london_crime)[names(london_crime) == "borough"] <- "Borough"
 names(london_crime)[names(london_crime) == "minor_category"] <- "SubCategory"
@@ -25,13 +26,13 @@ london_crime
 str(london_crime)
 
 ###################################
-
+#question-3
 london_crime$CrimeDate <- as.Date(converted_date, "%d/%m/%Y")
 london_crime
 str(london_crime)
 
 ###################################
-
+#question-4
 london_crime$Borough <- factor(london_crime$Borough)
 str(london_crime)
 
@@ -47,7 +48,7 @@ detach(london_crime)
 # City of London --   86 -- lowest  
 
 ###################################
-
+#question-5
 london_crime$MajorCategory <- factor(london_crime$MajorCategory)
 
 attach(london_crime)
@@ -60,19 +61,18 @@ pie(MajorCategory,  main="The percentage of crime by major category")
 detach(london_crime)
 
 ###################################
-
+#question-6
 london_crime$Region[Borough == "Kingston upon Thames" | Borough == "Newham" | Borough == "Barking and Dagenham" | Borough == "Bexley" | Borough == "Greenwich" | Borough == "Havering"| Borough == "Redbridge" | Borough == "Wandsworth" ] <- "East"
 london_crime$Region[Borough == "Barnet" | Borough == "Enfield" | Borough == "Hackney" | Borough == "Haringey" ] <- "North"
 london_crime$Region[Borough == "Brent" | Borough == "Ealing" | Borough == "Hammersmith and Fulham" | Borough == "Harrow" | Borough == "Hillingdon" | Borough == "Hounslow" | Borough == "Richmond upon Thames"] <- "West"
 london_crime$Region[Borough == "Bromley" | Borough == "Croydon" | Borough == "Merton" | Borough == "Sutton" ] <- "South"
 london_crime$Region[Borough == "Islington" | Borough == "Kensington and Chelsea" | Borough == "Lambeth" | Borough == "Lewisham" | Borough == "Southwark" | Borough == "Waltham Forest" | Borough == "Tower Hamlets" | Borough == "Westminster" ] <- "Central"
-#Checking for nulls
-which(london_crime$MajorCategory == NA)
+
 #City of london is nA so filling the values
 london_crime$Region[Borough == "City of London"] <- "Central"
 
 ######################################
-
+#qurstion-7
 london_crime$Region <- factor(london_crime$Region)
 str(london_crime)
 plot(london_crime$Region)
@@ -80,8 +80,36 @@ summary(london_crime$Region)
 
 #Central is the highest one is central
 # South is the least one
-###################################
+#####################################
+#question-8
+#the major crime category of both regions
+highest_crime_dataset <- subset(london_crime_modified, Region == "Central" )
+# lowest crime region
+lowest_crime_dataset <- subset(london_crime_modified, Region == "South" )
 
+
+#####################################
+#question-9
+opar <- par(no.readonly = TRUE)
+par = opar
+
+par(mfrow=c(1,2))
+plot(highest_region$MajorCategory, main="Number Of Crimes Committed By Region")
+plot(lowest_region$MajorCategory, main="Number Of Crimes Committed By Region")
+
+#Currently the Y Axis are different on both charts so I will set a range
+graph_range <- range(0, 9000)
+graph_range
+
+# Both graphs will now have a max of 9000 on the Y axis
+par(mfrow=c(1,2))
+plot(highest_region$MajorCategory, main="Category Of Crimes Committed In Central London", 
+     ylab="# Of Crimes", xlab="Category Of Crime", las=3, ylim=graph_range)
+plot(lowest_region$MajorCategory, main="Category Of Crimes Committed In South London", 
+     ylab="# Of Crimes", xlab="Category Of Crime", las=3, ylim=graph_range)
+
+#####################################
+#question-10
 write.csv(london_crime,"london-crime-modified.csv")
 
 
